b85c3154582f3447363a5215e5cbf91c05816468
1000ch <shogo.sensui@gmail.com>
2018-03-09 00:37:18 +0900

    Use Service Worker to cache assets

---
+++ static/sw.js
@@ -0,0 +1,42 @@
+const CACHE_KEY = '20180309';
+const CACHE_FILES = [
+  'background.jpg',
+  'components.css',
+  'font.css',
+  'base.css',
+  'NotoSansCJKjp-Bold.woff2',
+  'NotoSansCJKjp-Regular.woff2'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', e => {
+  const deletion = caches.keys()
+    .then(keys => keys.filter(key => key !== CACHE_KEY))
+    .then(keys => Promise.all(keys.map(key => caches.delete(key))));
+
+  e.waitUntil(deletion.then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  if (!CACHE_FILES.some(file => e.request.url.includes(file))) {
+    return;
+  }
+
+  const cache = caches.match(e.request).then(response => {
+    return response || fetch(e.request.clone()).then(response => {
+      if (response.ok) {
+        const clone = response.clone();
+        caches.open(CACHE_KEY).then(cache => cache.put(e.request, clone));
+      }
+
+      return response;
+    });
+  });
+
+  e.respondWith(cache);
+});
